e6291224a14acde0b0fdf26e327883ad1b432f91
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-16 23:36:48 +0300

    Task3 solved

---
+++ task3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Написать программу копирования массива
+
+void FillArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-5, 10);
+    }
+
+}
+
+void PrintArr(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+
+    Console.WriteLine();
+}
+
+int[] CopyArr(int[] arr)
+{
+    int[] arr_copy = new int[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr_copy[i] = arr[i];
+    }
+
+    return arr_copy;
+
+}
+
+int[] first_array = new int[10];
+
+FillArr(first_array);
+
+Console.WriteLine("Массив");
+
+PrintArr(first_array);
+
+int[] second_array = CopyArr(first_array);
+
+Console.WriteLine("Копия массива");
+
+PrintArr(second_array);
